aee285f75934cd24e68373da6b4ba9c5f906015c
kendlc <kennethcdlc@gmail.com>
2022-05-28 14:26:41 +1000

    show more button for pokedex

--- src/components/GenI.js
+++ src/components/GenI.js
@@ -4,17 +4,22 @@ import { Card,Button, Container, Modal, Row, Col } from "react-bootstrap";
 
 const GenI = () => {
     const [pokemon, setPokemon] = useState([]);
+    const [pokemon2, setPokemon2] = useState([]);
+
     const [modalShow, setModalShow] = useState(false);
     const [pokeData, setPokeData] = useState('');
 
+    const [switch2, setSwitch2] = useState(false);
+
     useEffect( () => {
-        if (pokemon.length <= 20){
-            getPoke()
+        if (pokemon.length <= 151){
+            getPoke();
+            getPoke2();
         }
     },[])
 
     const getPoke =  () => {
-        for (let i = 1; i <= 20; i++ ){
+        for (let i = 1; i <= 78; i++ ){
             axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`).then(
                 async (result) => {
                    await setPokemon((prev) => ([ ...prev, result.data ]))
@@ -23,7 +28,19 @@ const GenI = () => {
         }
     }
 
+    const getPoke2 =  () => {
+        for (let i = 79; i <= 151; i++ ){
+            axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`).then(
+                async (result) => {
+                   await setPokemon2((prev) => ([ ...prev, result.data ]))
+                }
+            )
+
+        }
+    }
+
     return (
+        <Container>
         <Container className="d-flex align-content-stretch flex-wrap">
             {
                 pokemon.sort().map( (pmon) => {
@@ -37,16 +54,46 @@ const GenI = () => {
                                 }>
 
                                 <Card.Img variant="bottom" className="align-self-center" style={{ objectFit: 'contain', padding: '.3rem'}} src={pmon.sprites.other["official-artwork"]["front_default"]} />                
-                                    <Card.Body className="align-top">
+                                    <Card.Body className="align-top p-2">
                                         <Card.Title className="align-text-center text-capitalize d-flex justify-content-center">{pmon.name}</Card.Title>
                                         <Container
                                         className="d-flex justify-content-center">
-                                            <Button variant="outline-primary" className="btn-sm " disabled>{
-                                                pmon.types[0].type.name.charAt(0).toUpperCase() + pmon.types[0].type.name.slice(1)
+                                            <Button variant="outline-primary" className="btn-sm text-capitalize mx-1" disabled>{
+                                                pmon.types[0].type.name
                                                 }</Button>
                                             { pmon.types[1] &&
-                                                <Button variant="outline-danger" className="btn-sm " disabled>{
-                                                    pmon.types[1].type.name.charAt(0).toUpperCase() + pmon.types[1].type.name.slice(1)
+                                                <Button variant="outline-danger" className="btn-sm text-capitalize" disabled>{
+                                                    pmon.types[1].type.name
+                                                    }</Button>
+                                            }
+                                        </Container>
+                                    </Card.Body>
+                                </Card>
+                            )   
+                })
+            }
+            { switch2 &&
+                pokemon2.sort().map( (pmon) => {
+                    return  (    
+                                <Card key={Math.random()}
+                                style={{ width: '11rem',height: '16rem', backgroundColor: 'rgba(255, 215, 255, 0.1)', margin: '1rem'  }}
+                                onClick={() => {
+                                    setPokeData(pmon)
+                                    setModalShow(true)
+                                    }
+                                }>
+
+                                <Card.Img variant="bottom" className="align-self-center" style={{ objectFit: 'contain', padding: '.3rem'}} src={pmon.sprites.other["official-artwork"]["front_default"]} />                
+                                    <Card.Body className="align-top p-2">
+                                        <Card.Title className="align-text-center text-capitalize d-flex justify-content-center">{pmon.name}</Card.Title>
+                                        <Container
+                                        className="d-flex justify-content-center">
+                                            <Button variant="outline-primary" className="btn-sm text-capitalize mx-1" disabled>{
+                                                pmon.types[0].type.name
+                                                }</Button>
+                                            { pmon.types[1] &&
+                                                <Button variant="outline-danger" className="btn-sm text-capitalize" disabled>{
+                                                    pmon.types[1].type.name
                                                     }</Button>
                                             }
                                         </Container>
@@ -61,20 +108,32 @@ const GenI = () => {
                 pdata={pokeData}
             />
         </Container>
+        <Container className="text-center">
+            <Button variant="secondary m-4 btn-lg"
+            onClick={ () => setSwitch2(true) }>Show More</Button>
+        </Container>
+        
+        </Container>
     );
 };
 
-function MyVerticallyCenteredModal(props) {
-    const [flavorText, setFlavorText] = useState([])
+const MyVerticallyCenteredModal = (props) => {
+    const [flavorText, setFlavorText] = useState('')
+    const [egg, setEgg] = useState([])
+    const [type, setType] = useState([])
+    const [genus, setGenus] = useState('')
 
     useEffect( () => {
         pokeEntry();
     },[props.pdata])
 
     const pokeEntry = () => {
-        axios.get(`https://pokeapi.co/api/v2/pokemon-species/${props.pdata.id || 'pikachu'}`)
+        axios.get(`https://pokeapi.co/api/v2/pokemon-species/${props.pdata.id || '151'}`)
         .then( ({data}) => {
-            setFlavorText(data);
+            setFlavorText(data.flavor_text_entries[71].flavor_text);
+            setEgg(data.egg_groups);
+            setType(props.pdata.types)
+            setGenus(data.genera[7].genus)
         })
     }
     
@@ -87,7 +146,7 @@ function MyVerticallyCenteredModal(props) {
       >
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
-                { props.pdata ? `# ${props.pdata.id} ${ props.pdata.name.charAt(0).toUpperCase() + props.pdata.name.slice(1) }` : '...'}
+                { props.pdata ? `# ${ props.pdata.id} ${ props.pdata.name.charAt(0).toUpperCase() + props.pdata.name.slice(1) }` : ''}
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
@@ -95,53 +154,46 @@ function MyVerticallyCenteredModal(props) {
                     <Col sm={3} 
                     className="d-flex justify-content-lg-end justify-content-center"
                     >
-                        <div style={{height: '10rem', objectFit: 'contain'}}>
-                            <img src={props.pdata ? props.pdata.sprites.versions["generation-v"]["black-white"].animated.front_default : ''}
+                        <div style={{ height: '10rem', objectFit: 'contain'}}>
+                            <img src={ props.pdata ? props.pdata.sprites.versions["generation-v"]["black-white"].animated.front_default : ''}
                             className="align-self-center img-fluid" 
                             style={{ width: '10rem', padding: '.3rem'}}
-                            // style={{transform: 'scale(2)'}}
-                            // className="img-fluid "
                             />
                         </div>
                     </Col>
                     <Col sm={9}>
-                        <h4>{props.pdata ? flavorText.genera[7].genus : '...'}</h4>
+                        <h4> {genus} </h4>
                         <p>
-                            <Button variant="outline-primary" className="btn-sm " disabled>{
-                                props.pdata ? props.pdata.types[0].type.name.charAt(0).toUpperCase() + props.pdata.types[0].type.name.slice(1) : '...'
+                            <Button variant="outline-primary" className="btn-sm text-capitalize" disabled>{
+                                props.pdata ? props.pdata.types[0].type.name : ''
                             }</Button>
                             { props.pdata ? 
                                 props.pdata.types[1] &&
-                                <Button variant="outline-danger" className="btn-sm " disabled>{
-                                    props.pdata.types[1].type.name.charAt(0).toUpperCase() + props.pdata.types[1].type.name.slice(1)
+                                <Button variant="outline-danger" className="btn-sm text-capitalize mx-2" disabled>{
+                                    props.pdata.types[1].type.name
                                     }</Button>
-                                : '...'
+                                : ''
                             }
                         </p>
-                        <p>
-                            {props.pdata ? flavorText['flavor_text_entries'][71]['flavor_text'] : '...'}
-                        </p>
+                        <p> { flavorText } </p>
                         <div
                         style={{ justifyContent: 'space-around'}}
                         >
                             <span>Egg Group:
-                                <Button variant="outline-secondary" className="btn-sm mx-1" disabled>
-                                    {props.pdata ?
-                                    flavorText.egg_groups[0].name.charAt(0).toUpperCase() + flavorText.egg_groups[0].name.slice(1)
-                                    : '...' }
-                                </Button>
-                                <Button variant="outline-secondary" className="btn-sm mx-1" disabled>
-                                    {props.pdata ?
-                                    flavorText.egg_groups[1].name.charAt(0).toUpperCase() + flavorText.egg_groups[1].name.slice(1)
-                                    : '...' }
-                                </Button >
+                                { egg.map( (e) => {
+                                    return (
+                                        <Button key={ Math.random() } variant="outline-secondary" className="btn-sm mx-1 text-capitalize" disabled>
+                                            {e.name}
+                                        </Button>
+                                    )
+                                })}
                             </span>
                             <span className="mx-5">
                                 Height:
                                 <Button variant="outline-secondary" className="btn-sm mx-1" disabled>
                                     {props.pdata ?
                                     props.pdata.height/10
-                                    : '...' } m
+                                    : '' } m
                                 </Button>
                             </span>
                         </div>
