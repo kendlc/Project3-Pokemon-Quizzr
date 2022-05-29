008ff4d7c1f85da750691aa1b73349b7414856e8
kendlc <kennethcdlc@gmail.com>
2022-05-30 09:50:48 +1000

    finished pokequiz image reveal

--- src/components/PokdeQuiz/QuizGenI.js
+++ src/components/PokdeQuiz/QuizGenI.js
@@ -1,47 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import ReactCSSTransitionGroup from "react-transition-group"
 
 const QuizGenI = () => {
-	const questions = [
-		{
-			questionText: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
-		},
-	];
 
     const [pokeQuest, setPokeQuest ] = useState([]);
+    const [show, setShow] = useState(false);
+    const [unmask, setUnmask] = useState(false);
 
     useEffect( () => {
         getPokeGuess();
@@ -74,16 +39,26 @@ const QuizGenI = () => {
 	const [score, setScore] = useState(0);
 
 	const handleAnswerOptionClick = (isCorrect) => {
-		if (isCorrect) {
-			setScore(score + 1);
-		}
+        // setShow(true);
+        // setTimeout( setShow(false), 2000);
+        // setTimeout( () => {
+            if (isCorrect) {
+                setScore(score + 1);
+            }
 
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < pokeQuest.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			setShowScore(true);
-		}
+            const nextQuestion = currentQuestion + 1;
+            if (nextQuestion < pokeQuest.length) {
+                setShow(true)
+                setUnmask(true)
+                setTimeout( () => {
+                setCurrentQuestion(nextQuestion);
+                setUnmask(false);
+                setShow(false);
+                },1500)
+            } else {
+                setShowScore(true);
+            }
+        // }, 3000)
 	};
 	return (
 		<div className='app'>
@@ -97,13 +72,19 @@ const QuizGenI = () => {
 						<div className='question-count'>
 							<span>Question {currentQuestion + 1}</span>/{pokeQuest.length}
 						</div>
+                        <ReactCSSTransitionGroup
+                        transitionName="example"
+                        transitionEnterTimeout={500}
+                        transitionLeaveTimeout={300}>
 						<div className='question-text mb-5'>
                             <img src={
                                 pokeQuest[currentQuestion] ?
                                 pokeQuest[currentQuestion].image
                                 : ''
-                            } />
-                            <div style={{minHeight: '120px'}}>
+                            } 
+                            className={unmask ? 'unmask' : 'mask'}
+                            />
+                            {/* <div style={{minHeight: '120px'}}>
                                 <div className="collapse collapse-horizontal" id="collapseWidthExample">
                                     <div className="card card-body" style={{width: '300px'}}>
                                         <h2>
@@ -115,8 +96,19 @@ const QuizGenI = () => {
                                         </h2>
                                     </div>
                                 </div>
-                            </div>
+                            </div> */}
+                            {show ?
+                                        <h2 className="answer">
+                                        {
+                                            pokeQuest[currentQuestion] ?
+                                            pokeQuest[currentQuestion].questionText
+                                        : ''
+                                        }
+                                        </h2>
+                                         : '' }
+                                         
                         </div>
+                        </ReactCSSTransitionGroup>
 					</div>
                     { pokeQuest[currentQuestion] && 
 					<div className='answer-section'>
@@ -127,7 +119,7 @@ const QuizGenI = () => {
 							<button 
                             key={Math.random()} 
                             onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
-                            class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample"
+                            // class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample"
                             >
                             {answerOption.answerText}
                             </button>
@@ -147,3 +139,43 @@ export default QuizGenI;
 // .map(value => ({ value, sort: Math.random() }))
 // .sort((a, b) => a.sort - b.sort)
 // .map(({ value }) => value)
+
+
+// const questions = [
+//     {
+//         questionText: 'What is the capital of France?',
+//         answerOptions: [
+//             { answerText: 'New York', isCorrect: false },
+//             { answerText: 'London', isCorrect: false },
+//             { answerText: 'Paris', isCorrect: true },
+//             { answerText: 'Dublin', isCorrect: false },
+//         ],
+//     },
+//     {
+//         questionText: 'Who is CEO of Tesla?',
+//         answerOptions: [
+//             { answerText: 'Jeff Bezos', isCorrect: false },
+//             { answerText: 'Elon Musk', isCorrect: true },
+//             { answerText: 'Bill Gates', isCorrect: false },
+//             { answerText: 'Tony Stark', isCorrect: false },
+//         ],
+//     },
+//     {
+//         questionText: 'The iPhone was created by which company?',
+//         answerOptions: [
+//             { answerText: 'Apple', isCorrect: true },
+//             { answerText: 'Intel', isCorrect: false },
+//             { answerText: 'Amazon', isCorrect: false },
+//             { answerText: 'Microsoft', isCorrect: false },
+//         ],
+//     },
+//     {
+//         questionText: 'How many Harry Potter books are there?',
+//         answerOptions: [
+//             { answerText: '1', isCorrect: false },
+//             { answerText: '4', isCorrect: false },
+//             { answerText: '6', isCorrect: false },
+//             { answerText: '7', isCorrect: true },
+//         ],
+//     },
+// ];
